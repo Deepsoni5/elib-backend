09c199954c1c6c013133a88dc09fea723c9b5a67
Deepsoni5 <fichadiyadeep@gmail.com>
2024-06-10 22:38:22 +0530

    add password hashing

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
-
+import bcrypt from "bcrypt";
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
    const { name, email, password } = req.body;
 
@@ -20,6 +20,7 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
    }
 
    // Process
+   const hashedPassword = await bcrypt.hash(password, 10);
 
    // Response
 
